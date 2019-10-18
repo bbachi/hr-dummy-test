955ec86b54ca7cd216706e8dcb3a9c8ba68b8e63
Bhargav Bachina <bhargavbachina@yahoo.com>
2019-10-18 10:57:25 -0500

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,29 +4,32 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-mocha-reporter'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/hackerrank'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    angularCli: {
+      environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    junitReporter: {
+      useBrowserName: false,
+      outputFile: 'unit.xml',
+      suite: 'unit'
+    }
   });
 };
